dfde84f4b47374a64fc4134a5ca5b77be63e1a13
Alex <ARICKERT0003@gmail.com>
2020-04-17 15:52:51 -0500

    Added documentation for Multimeter and PathHandler

--- Multimeter.h
+++ Multimeter.h
@@ -1,16 +1,35 @@
+/** 
+ *  @file    Multimeter.h 
+ *  @author  Alex Rickert
+ *  @date    04/02/2020
+ *  @version 1.0
+ */
+
 #ifndef MULTIMETER
 #define MULTIMETER 
 
 #include <Arduino.h>
 #include "SCPICommands.h"
 #include "CSVData.h"
-  
+
+/**
+ *  @enum  MM_ERROR
+ *  @brief Error codes for multimeter
+ *  @var   NoError
+ *  @var   SerialWriteError
+ */
 enum MM_ERROR
 {
   NoError = 0,
   SerialWriteError = 1000
 };
 
+/**
+ *  @enum  MM_MODE
+ *  @brief Modes for multimeter
+ *  @var   VoltageDC
+ *  @var   CurrentDC
+ */
 enum MM_MODE
 {
   UNSET = -1,
@@ -18,6 +37,21 @@ enum MM_MODE
   CurrentDC = 1
 };
 
+/**
+ *  @enum  MM_COMMAND
+ *  @brief Commands for multimeter 
+ *  @var   Restart  
+ *         Restart the multimeter
+ *  @var   Trigger  
+ *         Forces multimeter to capture new value
+ *  @var   Identify 
+ *         Returns multimeter information
+ *  @var   Fetch    
+ *         Returns last data point multimater captured
+ *  @var   Function 
+ *         Returns mode multimeter is in (ie VoltageDC, VoltageAC, CurrentDC,
+ *         ..)
+ */
 enum MM_COMMAND
 {
   Restart = 0,
@@ -29,26 +63,84 @@ enum MM_COMMAND
 
 /** 
  *  @class Multimeter
- *  @brief Gets data from multimeter
+ *  @brief Gets data from multimeter via SCPI protocol\n
+ *         This is tailored to BK Precision 5491B Multimeter
  */  
 class Multimeter : public SCPI
 {
   public:
 
+  /**
+   *  @struct MeterMode
+   *  @brief  Contains strings to compare and validate meter mode
+   */
   struct MeterMode
   {
     char* mode;
     unsigned int len;
   };
 
+  /**
+   *  @fn    Multimeter
+   *  @brief Empty constructor
+   */
   Multimeter();
+
+  /**
+   *  @fn    ~Multimeter
+   *  @brief Empty destructor
+   */
   ~Multimeter();
+
+  /**
+   *  @fn    init
+   *  @brief Initializes arrays with apropriate commands and modes 
+   */
   void init();
+
+  /**
+   *  @fn    transmit
+   *  @brief Sends command to multimeter
+   *  @param Command @ref MM_COMMAND
+   */
   int transmit(int);
+
+  /**
+   *  @fn    receive
+   *  @brief Waits for response from multimeter and stores in 
+   *         command specfic buffer
+   *  @param Command @ref MM_COMMAND
+   */
   int receive(int);
+
+  /**
+   *  @fn    receive
+   *  @brief Waits for response from multimeter and stores in 
+   *         general buffer
+   */
   void receive();
+
+  /**
+   *  @fn    execute
+   *  @brief Sends command and waits for response. Stores response in
+   *         command specific buffer
+   */
   int execute(int);
+
+  /**
+   *  @fn    execute
+   *  @brief Sends command, waits for response, stores response in
+   *         CSVData instance
+   *  @param Command @ref MM_COMMAND
+   *  @param ptrCSVData @ref CSVDATA
+   *  @param @deprecated meterMode
+   */
   int execute(int, CSVData*, int);
+
+  /**
+   *  @fn    validateMode
+   *  @brief NOT FUNCTIONAL YET
+   */
   int validateMode(int);
 
   private:
--- PathHandler.h
+++ PathHandler.h
@@ -11,6 +11,12 @@
 #include <SPI.h>
 #include <SD.h>
 
+/**
+ *  @enum PH_ERROR
+ *  @var  PH_NO_ERROR
+ *  @var  PH_CANNOT_LOCATE_SD
+ *  @var  PH_CANNOT_OPEN_FILE
+ */
 enum PH_ERROR
 {
   PH_NO_ERROR = 0,
@@ -19,22 +25,99 @@ enum PH_ERROR
 };
 
 /**  
- *  @class PathHandler
- *  @brief Handles opening and closing of SD module and file structure creation
+ *  @class   PathHandler
+ *  @brief   Handles opening and closing of SD module and file structure creation
+ *  @details This class wants to be similar to std::filesystem::path but needs
+ *           a lot of work. The general idea is to be able to dynamically
+ *           create and alter paths in a clean and efficient way. Users need
+ *           not worry about syntax and should be able to swap out any segment
+ *           without altering the rest of the path.  
  */
 class PathHandler
 {
   public:
+
+  /**
+   *  @fn     PathHandler
+   *  @brief  Empty constructor
+   */
   PathHandler();
+
+  /**
+   *  @fn     init
+   *  @brief  Initializes pointers and opens SD 
+   *  @param  pin Pin which SD writer is using
+   *  @return error @ref PH_ERROR
+   */
   int init(byte);
+
+  /**
+   *  @fn      init
+   *  @brief   Same as above but also populates path with values
+   *  @details Each segment of path should be 8 characters long. This will
+   *           eventually be changed.
+   *  @param   pin
+   *  @param   baseDir
+   *  @param   branchDir
+   *  @param   fileName
+   *  @return  error @ref PH_ERROR
+   */
   int init(byte, char*, char*, char*);
+
+  /**
+   *  @fn      makePath
+   *  @brief   Makes base directory, branch directory, and file name
+   *  @details Each segment of path should be 8 characters long. This will
+   *           eventually be changed.  
+   *  @param   baseDir Base Directory
+   *  @param   branchDir Branch directory
+   *  @param   fileName Name of file
+   */
   void makePath(char*, char*, char*);
+
+  /**
+   *  @fn      setFilePath
+   *  @brief   Should be protected / private
+   */
   void setFilePath(char*, char*, char*);
+
+  /**
+   *  @deprecated
+   *  @fn      printPaths
+   *  @brief   Should be deleted
+   */
   void printPaths();
+
+  /**
+   *  @fn      formatFilePath
+   *  @brief   Should be protected / private
+   */
   void formatFilePath();
+
+  /**
+   *  @fn      setDirectory
+   *  @brief   Should be protected / private
+   */
   void setDirectory();
+
+  /**
+   *  @fn      getFile
+   *  @brief   Returns pointer to file at end of path
+   *  @return  ptrFile @ref File
+   */
   File* getFile();
+
+  /**
+   *  @fn      openFile 
+   *  @brief   Opens file on SD in write mode
+   *  @return  error @ref PH_ERROR
+   */
   int openFile();    
+
+  /**
+   *  @fn      closeFile
+   *  @brief   Closes file on SD
+   */
   void closeFile();
 
   private:
